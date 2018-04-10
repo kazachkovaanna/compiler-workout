29c7e9d676329a16ed67b73020f00af06c0e505b
kazachkovaanna <kazachkova.ann@gmail.com>
2018-04-10 23:40:53 +0300

    hw6

--- src/Language.ml
+++ src/Language.ml
@@ -15,21 +15,26 @@ module State =
     type t = {g : string -> int; l : string -> int; scope : string list}
 
     (* Empty state *)
-    let empty = failwith "Not implemented"
+    let fail = failwith "Undefined var"
+    let empty = { g = fail; l = fail; scope = []}
 
     (* Update: non-destructively "modifies" the state s by binding the variable x 
        to value v and returns the new state w.r.t. a scope
     *)
-    let update x v s = failwith "Not implemented"
+    let update x v s = 
+      let update' x v s = fun y-> if x = y then v else s y in
+        if List.mem x s.scope
+          then {s with l = update' x v s.l}
+          else {s with g = update' x v s.g}
                                 
     (* Evals a variable in a state w.r.t. a scope *)
-    let eval s x = failwith "Not implemented" 
+    let eval s x = if List.mem x s.scope then (s.l x) else (s.g x)
 
     (* Creates a new scope, based on a given state *)
-    let enter st xs = failwith "Not implemented"
+    let enter st xs = {st with g = st.g; scope = xs;}
 
     (* Drops a scope *)
-    let leave st st' = failwith "Not implemented"
+    let leave st st' = {st' with g = st.g}
 
   end
     
@@ -60,7 +65,31 @@ module Expr =
        Takes a state and an expression, and returns the value of the expression in 
        the given state.
     *)                                                       
-    let eval st expr = failwith "Not implemented"      
+    let evalBinaryOperation operation exprLeft exprRight =
+      let booleanOfInt value = if value = 0 then false else true in
+      let intOfBoolean value = if value = true then 1 else 0 in
+      match operation with
+      | "+" -> exprLeft + exprRight
+      | "-" -> exprLeft - exprRight
+      | "*" -> exprLeft * exprRight
+      | "/" -> exprLeft / exprRight
+      | "%" -> exprLeft mod exprRight
+      | "==" -> intOfBoolean(exprLeft == exprRight)
+      | "!=" -> intOfBoolean(exprLeft != exprRight)
+      | "<"  -> intOfBoolean(exprLeft < exprRight)
+      | "<=" -> intOfBoolean(exprLeft <= exprRight)
+      | ">"  -> intOfBoolean(exprLeft > exprRight)
+      | ">=" -> intOfBoolean(exprLeft >= exprRight)
+      | "&&" -> intOfBoolean(booleanOfInt exprLeft && booleanOfInt exprRight)
+      | "!!" -> intOfBoolean(booleanOfInt exprLeft || booleanOfInt exprRight)
+      | _ -> failwith "Wrong operation"
+    
+    let rec eval state expr =
+      let eval' = eval state in
+      match expr with
+      | Const value -> value
+      | Var var -> state var
+      | Binop(op, l, r) -> evalBinaryOperation op (eval' l) (eval' r)
 
     (* Expression parser. You can use the following terminals:
 
@@ -68,8 +97,27 @@ module Expr =
          DECIMAL --- a decimal constant [0-9]+ as a string
                                                                                                                   
     *)
-    ostap (                                      
-      parse: empty {failwith "Not implemented"}
+
+    let elementsOperation operationsList = let binOperationList op = (ostap ($(op)), fun l r -> Binop (op, l, r))
+      in List.map binOperationList operationsList;;
+
+    ostap (
+      parse:
+        !(Ostap.Util.expr
+          (fun x -> x)
+          [|
+            `Lefta, elementsOperation ["!!"];
+            `Lefta, elementsOperation ["&&"];
+            `Nona,  elementsOperation [">="; ">"; "<="; "<"; "=="; "!="];
+            `Lefta, elementsOperation ["+"; "-"];
+            `Lefta, elementsOperation ["*"; "/"; "%"]
+          |]
+          primary
+        );
+      primary: 
+        var:IDENT {Var var} 
+        | literal:DECIMAL {Const literal} 
+        | -"(" parse -")"
     )
     
   end
@@ -104,11 +152,66 @@ module Stmt =
 
        which returns a list of formal parameters, local variables, and a body for given definition
     *)
-    let eval env ((st, i, o) as conf) stmt = failwith "Not implemented"
+    let rec zip = function
+      | x::xs, y::ys -> (x, y) :: (zip (xs, ys))
+      | [], [] -> []
+      | _, _ -> failwith "Not even number"
+
+    let rec eval env ((state, input, output) as conf) statement = 
+      match statement with
+      | Assign (var, expr) -> (State.update var (Expr.eval state expr) state, input, output)
+      | Read (var) -> 
+        (match input with
+        | list::tail -> (State.update var list state, tail, output)
+        | [] -> failwith "Empty input stream")
+      | Write (expr) -> (state, input, output @ [(Expr.eval state expr)])
+      | Seq (firstStatement, secondStatement) -> (eval (eval (state, input, output) firstStatement ) secondStatement)
+      | Skip -> (state, input, output)
+      | If (expr, firstStatement, secondStatement) -> if Expr.eval state expr != 0 
+        then
+          eval (state, input, output) firstStatement 
+        else 
+          eval (state, input, output) secondStatement
+      | While (expr, stmt) -> if Expr.eval state expr !=0 
+        then
+          eval (eval (state, input, output) stmt) statement
+        else 
+          (state, input, output)
+      | Repeat (expr, stmt) -> 
+        let (state', input', output') = eval (state, input, output) stmt in
+          if Expr.eval state' expr == 0 
+            then 
+              eval(state', input', output') statement
+            else
+              (state', input', output')
+      |Call (funcName, args) ->
+        let params, fvars, body = env#definition funcName in
+        let evaluatedArgs = List.map (Expr.eval st) args in
+        let assign st (variable, value) = State.update variable value st in
+        let startState = State.enter st (params @ fvars) in
+        let localState = List.fold_left assign startState(zip(params, evaluatedArgs)) in
+        let endState, newInput, newOutput = eval env (localState, input, output) body in
+        (State.leave endState st, newInput, newOutput)
+      | _ -> failwith "Error: not a statement" 
                                 
     (* Statement parser *)
     ostap (
-      parse: empty {failwith "Not implemented"}
+      parse: sequence | statement;
+      statement: read | write | assign | if_ | while_ | for_ | repeat_ | skip | call;
+      read: "read" -"(" var:IDENT -")" {Read var};
+      write: "write" -"(" expr:!(Expr.parse) -")" { Write expr };
+      assign: var:IDENT -":=" expr:!(Expr.parse) { Assign (var, expr) };
+      if_: "if" expr:!(Expr.parse) "then" stmt:parse "fi" {If (expr, stmt, Skip)}
+         | "if" expr:!(Expr.parse) "then" stmt1:parse  else_elif:else_or_elif "fi" {If (expr, stmt1, else_elif)};
+      else_or_elif : else_ | elif_;
+      else_: "else" stmt:parse {stmt};
+      elif_: "elif" expr:!(Expr.parse) "then" stmt1:parse stmt2:else_or_elif "fi" {If (expr, stmt1, stmt2)};
+      while_: "while" expr:!(Expr.parse) "do" stmt:parse "od" {While (expr, stmt)};
+      for_: "for" initial:parse "," expr:!(Expr.parse) "," stmt1:parse "do" stmt2:parse "od" {Seq (initial, While(expr, Seq(stmt1, stmt2)))};
+      repeat_ : "repeat" stmt:parse "until" expr:!(Expr.parse) {Repeat (expr, stmt)};
+      skip: "skip" {Skip};
+      sequence: firstStatement:statement -";" secondStatement:parse { Seq (firstStatement, secondStatement) };
+      call: var:IDENT "(" args:!(Util.list0)[Expr.parse] ")" {Call (var, args)}
     )
       
   end
@@ -121,7 +224,10 @@ module Definition =
     type t = string * (string list * string list * Stmt.t)
 
     ostap (
-      parse: empty {failwith "Not implemented"}
+      parse: %"fun" funcName:IDENT "(" args:(names | none) ")" vars:(-(%"local") names | none)
+        "{" body:!(Stmt.parse) "}" { funcName, (args, vars, body) } ;
+      none : empty{ [] };
+      names: head:IDENT tail:((-"," IDENT)* ) {head :: tail}
     )
 
   end
@@ -137,7 +243,16 @@ type t = Definition.t list * Stmt.t
 
    Takes a program and its input stream, and returns the output stream
 *)
-let eval (defs, body) i = failwith "Not implemented"
+let eval (defs, body) i = 
+  let module Module = Map.Make (String) in
+  let defsMap = List.fold_left (fun map (f, data) -> Module.add f data map) Module.empty defs in
+  let env = (object method defenition f = Module.find f defsMap end) in
+  let _, _, output = Stmt.eval env (State.empty, i, []) body in 
+  output
+
                                    
 (* Top-level parser *)
-let parse = failwith "Not implemented"
+ostap (
+  defs: !(Definition.parse) * ;
+  parse: defs:defs body:!(Stmt.parse) {defs, body}
+)
--- src/SM.ml
+++ src/SM.ml
@@ -31,7 +31,38 @@ type config = (prg * State.t) list * int list * Stmt.config
    Takes an environment, a configuration and a program, and returns a configuration as a result. The
    environment is used to locate a label to jump to (via method env#labeled <label_name>)
 *)                         
-let eval env ((cstack, stack, ((st, i, o) as c)) as conf) = failwith "Not implemented"
+let rec eval env ((cstack, stack, ((st, i, o) as c)) as conf) = function
+  | [] -> conf
+  | JMP name :: _ -> eval env conf (env#labeled name)
+  | CJMP (cond, name) :: prg' -> 
+    (match stack with
+      | (x::new_stack) -> 
+          (if (cond = "z" && x = 0) || (cond = "nz" && x <> 0) 
+            then eval env (cstack, new_stack, c) (env#labeled name)
+            else eval env (cstack, new_stack, c) prg'
+          )
+      | _ -> failwith "Empty stack"
+    )
+  | CALL name :: prg_next ->  eval env ((prg_next, st)::cstack, stack, c)(env#labeled name)
+  | END :: _-> let (prg_prev, st_prev)::cstack_new = cstack in
+                eval env (cstack_new, stack, (State.leave st st_prev, i, o)) prg_prev
+  | insn :: prg' ->
+    let newConfig = match insn with
+        | BINOP op -> let y::x::stack' = stack in (cstack, Expr.to_func_renamed op x y :: stack', c)
+        | READ     -> let z::i'        = i     in (cstack, z::stack, (st, i', o))
+        | WRITE    -> let z::stack'    = stack in (cstack, stack', (st, i, o @ [z]))
+        | CONST i  -> (cstack, i::stack, c)
+        | LD x     -> (cstack, State.eval st x :: stack, c)
+        | ST x     -> let z::stack' = stack in (cstack, stack', (State.update x z st, i, o))
+        | LABEL name -> conf
+        | BEGIN (p, l) -> let st_enter = State.enter st (p @ l) in
+                let stack_to_st = fun p (st, x::stack_next) ->  (State.update p x st, stack_next) in
+                let (st_enter, _) = List.fold_right stack_to_st
+                p (st_enter, stack) in
+                (cstack, stack, (st_enter, i, o))
+        | _ -> failwith "Smth wrong!"
+    in eval env newConfig prg'
+ 
 
 (* Top-level evaluation
 
@@ -56,4 +87,54 @@ let run p i =
    Takes a program in the source language and returns an equivalent program for the
    stack machine
 *)
-let compile (defs, p) = failwith "Not implemented"
+let createLabel number = "L_" ^ (string_of_int number)
+
+let compile (defs, p) =
+  let rec expr = function
+    | Expr.Var   x          -> [LD x]
+    | Expr.Const n          -> [CONST n]
+    | Expr.Binop (op, x, y) -> expr x @ expr y @ [BINOP op]
+  in
+  let rec compileStatement count = function  
+    | Stmt.Read x ->  (count, [READ; ST x])
+    | Stmt.Write e -> (count, expr e @ [WRITE])
+    | Stmt.Assign (x, e) -> (count, expr e @ [ST x])
+    | Stmt.Skip -> (count, [])
+    | Stmt.Seq (firstStatement, secondStatement) -> 
+        let (firstCondition, fistBody) = compileStatement count firstStatement in
+        let (secondStatement, secondBody) = compileStatement firstCondition secondStatement in
+        (secondStatement, fistBody @ secondBody)                                     
+    | Stmt.If (cond, firstStatement, secondStatement) -> 
+        let firstCondition, fistBody = compileStatement count firstStatement in
+        let thenLabel = createLabel firstCondition in
+        let secondStatement, secondBody = compileStatement (firstCondition+1) secondStatement in
+        let elseLabel = createLabel secondStatement in
+        (secondStatement+1, expr cond @ [CJMP ("z", thenLabel)] @ fistBody @ [JMP elseLabel; LABEL thenLabel] @ secondBody @ [LABEL elseLabel])                        
+    | Stmt.While (cond, st) -> 
+        let loopLabel = createLabel count in
+        let (firstCondition, fistBody) = compileStatement (count+1) st in
+        let checkLabel = createLabel firstCondition in
+        (firstCondition+1, [JMP checkLabel; LABEL loopLabel] @ fistBody @ [LABEL checkLabel] @ expr cond @ [CJMP ("nz", loopLabel)])
+    | Stmt.Repeat (st, cond) ->  
+        let loopLabel = createLabel count in
+        let (firstCondition, fistBody) = compileStatement (count+1) st in
+        (firstCondition, [LABEL loopLabel] @ fistBody @ expr cond @ [CJMP ("z", loopLabel)])
+    | Stmt.Call (name, args) -> let progArgs = List.concat @@ List.map expr args in
+        (count, progArgs @ [CALL ("L_" ^ name)])
+  in 
+  let compileDefinition countR (name, (params, locals, body)) =
+    let (firstCondition, progFunc) = compileStatement countR body in
+    (firstCondition, [LABEL name; BEGIN (params, locals)] @ progFunc @ [END])
+  in
+  let startCount = 0
+  in
+  let countStart, defsProg = List.fold_left
+      (fun (counter, prgs)(name, configur) -> 
+          let (countZ, progNew) = compileDefinition counter ("L_" ^ name, configur) 
+          in (countZ, progNew::prgs))
+      (startCount, [])
+      defs
+  in
+  let (_, prg) = compileStatement countStart p in
+  let mainLabel = "L_main"
+  in ([JMP mainLabel] @ (List.concat defsProg) @ [LABEL mainLabel] @ prg)
